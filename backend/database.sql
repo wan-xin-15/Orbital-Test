40fe0910aec353b3db0b99152037ef0f9f81e465
tze-xuan <ktzexuan@gmail.com>
2025-05-30 21:13:29 +0800

    Add database

---
+++ backend/database.sql
@@ -0,0 +1,15 @@
+CREATE DATABASE CafeChronicles;
+
+USE CafeChronicles;
+
+CREATE TABLE cafes (
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    cafeName VARCHAR(255) UNIQUE NOT NULL,
+    cafeLocation VARCHAR(255) NOT NULL
+);
+
+CREATE TABLE login(
+    id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(255) UNIQUE NOT NULL,
+    password VARCHAR(255) NOT NULL
+);
