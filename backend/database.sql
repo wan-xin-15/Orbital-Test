6d0417687202fcc0c5c5e84ebc0df9595d3a7c4a
tze-xuan <ktzexuan@gmail.com>
2025-06-01 22:22:19 +0800

    Add users table to sql file

--- backend/database.sql
+++ backend/database.sql
@@ -7,8 +7,9 @@ CREATE TABLE cafes (
     cafeLocation VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE login(
+CREATE TABLE users(
     id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(255) UNIQUE NOT NULL,
+    email VARCHAR(255) UNIQUE NOT NULL,
     password VARCHAR(255) NOT NULL
 );
