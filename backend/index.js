f902081339c023b638df3c53772be260a3f0b861
tze-xuan <ktzexuan@gmail.com>
2025-06-01 19:44:07 +0800

    Revert "Change in dependencies"

--- backend/index.js
+++ backend/index.js
@@ -23,11 +23,11 @@ app.get("/getData", (req, res) => {
 //CREATE
 app.post("/cafes", async (req, res) => {
   try {
-    const { cafeName, cafeLocation } = req.body;
+    const { name, location } = req.body;
     console.log(req.body);
     const newCafe = await pool.query(
-      "INSERT INTO cafes (cafeName, cafeLocation) VALUES (?, ?)",
-      [cafeName, cafeLocation]
+      "INSERT INTO cafes (name, location) VALUES (?, ?)",
+      [name, location]
     );
     res.json("Café added");
   } catch (err) {
@@ -60,11 +60,11 @@ app.get("/cafes/:id", async (req, res) => {
 app.put("/cafes/:id", async (req, res) => {
   try {
     const { id } = req.params;
-    const { cafeName, cafeLocation } = req.body;
+    const { name, location } = req.body;
 
     const updateCafe = await pool.query(
-      "UPDATE cafes SET cafeName = ?, cafeLocation = ? WHERE id = ?",
-      [cafeName, cafeLocation, id]
+      "UPDATE cafes SET name = ?, location = ? WHERE id = ?",
+      [name, location, id]
     );
     res.json("Café was updated");
   } catch (err) {
--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,9 +1,28 @@
+import React, {useState, useEffect} from "react";
+import Axios from "axios";
 import Login from "./pages/Login.tsx";
 import Home from "./pages/Home.tsx";
 import SignUp from "./pages/SignUp.tsx";
 import Community from "./pages/Community.tsx";
 import Cafes from "./pages/Cafes.tsx";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
+const Apps = () => {
+  const [data, setData] = useState("");
+
+  const getData = async() => {
+    const response = await Axios.get("http://localhost:5000/getData");
+    setData(response.data);
+  }
+
+useEffect(()=>{
+  getData()
+},[]);
+
+return (
+  <div>{data}</div>
+)
+}
 
 function App() {
   return (
